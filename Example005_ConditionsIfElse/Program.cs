8cfdebc8624e1bced8a80df3ddf83a91fa722087
spam423 <zombe423@list.ru>
2023-09-19 11:44:01 +0300

    Example005_Conditions If-Else

---
+++ Example005_ConditionsIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "ника")
+{
+    Console.WriteLine("Ура это же Ника!");
+}
+else    
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
